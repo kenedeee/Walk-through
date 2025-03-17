1c17d9538e6e8c43cac29c687990c30c598af74b
kenedeee <kenkenigayac@gmail.com>
2025-03-17 20:11:14 +0800

    Delete Walk-through 1: Basic Syntax and Data Handling directory

--- Walk-through 1: Basic Syntax and Data Handling/guidedTask1.java
+++
@@ -1,6 +0,0 @@
-public class guidedTask1{
-    public static void main(String[] args){
-        String fullName = "Kenedy \" B. Igayac";
-        System.out.println("Hello, " + fullName + "!");
-    }
-}
