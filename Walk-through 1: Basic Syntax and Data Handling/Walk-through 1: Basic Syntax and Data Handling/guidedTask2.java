037064258a7371b0d8d5ab1735e583d2dfe5ee82
kenedeee <kenkenigayac@gmail.com>
2025-03-17 20:06:34 +0800

    Create guidedTask2.java

---
+++ Walk-through 1: Basic Syntax and Data Handling/Walk-through 1: Basic Syntax and Data Handling/guidedTask2.java
@@ -0,0 +1,9 @@
+public class guidedTask2{
+    public static void main(String[] args) {
+        int length = 3;
+        int width = 4;
+        int area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
